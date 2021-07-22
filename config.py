d743a50d4d02258fc5d66ad54669f54fc4f7a225
bufferbandit <kernelland@protonmail.ch>
2021-07-22 23:02:10 +0200

    Added password auth to the web panel

--- config.py
+++ config.py
@@ -5,3 +5,7 @@
 # unicorn used to convert powershell agent into vba macro
 
 unicorn_path = "/opt/redteaming/unicorn-3.15/unicorn.py"
+
+
+basic_http_username = "octopus"
+basic_http_password = "e9394cecde3a038a82e922892e45436f" # octopus_password
